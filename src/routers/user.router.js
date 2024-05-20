ed570abea8f71dae5e09ddfbe9f81902af6a9853
Animesh <2025.cse18.animeshbag@gmail.com>
2024-05-20 21:02:42 +0530

    Added: multer confuguration for deployment issues

--- src/routers/user.router.js
+++ src/routers/user.router.js
@@ -2,9 +2,9 @@
 import { Router } from 'express';
 import multer from 'multer';
 import { uploadUsers } from '../controllers/user.controller.js';
+import upload from '../services/multer.service.js'
 
 const router = Router();
-const upload = multer({ dest: '/src/uploads/' });
 
 router.post('/:listId/upload', upload.single('file'), uploadUsers);
 
---
+++ src/services/multer.service.js
@@ -0,0 +1,22 @@
+import multer from 'multer';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Multer configuration
+const uploadDirectory = path.join(__dirname, '../uploads');
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, uploadDirectory);
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
+
+const upload = multer({ storage });
+
+export default upload;
