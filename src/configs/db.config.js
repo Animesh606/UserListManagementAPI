e6026a473fdd30510d2b510219c3263c410c9cc5
Animesh <2025.cse18.animeshbag@gmail.com>
2024-05-20 13:25:41 +0530

    Added: Database connection

---
+++ src/configs/db.config.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+import { db_url } from '../constants.js';
+
+const connectDB = async () => {
+    try {
+        console.log(db_url);
+        const conn = await mongoose.connect(`${db_url}`);
+        console.log('Database connected', conn.connection.host);
+    } catch (error) {
+        console.log('Connection failed', error);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
